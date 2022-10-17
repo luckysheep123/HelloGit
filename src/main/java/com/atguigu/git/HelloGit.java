57352df1661392165060b9d3e4972488738ed987
luckysheep123 <1658694310@qq.com>
2022-10-17 21:15:08 +0800

    添加文件

---
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author： 杨魏忠
+ * @date： 2022-10-17 20:52
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
